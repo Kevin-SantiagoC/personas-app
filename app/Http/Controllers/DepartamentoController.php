420c2214a33c2c2ce5c19e5a6134898fea66cd23
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-10-02 01:19:31 -0500

    Departamento Add

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -26,7 +26,10 @@ class DepartamentoController extends Controller
      */
     public function create()
     {
-        //
+        $paises =DB::table('tb_pais')
+        ->orderBy('pais_nomb')
+        ->get();
+        return view('departamento.new',['paises' => $paises]);
     }
 
     /**
@@ -34,7 +37,20 @@ class DepartamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $departamento = new Departamento();
+
+        $departamento->depa_nomb = $request->name;
+        $departamento->depa_codi = $request->code;
+        $departamento->save();
+
+        $departamentos = DB::table('tb_departamento')
+        ->join('tb_pais', 'tb_departamento.pais_codi','=','tb_pais.pais_codi')
+        ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+        ->get();
+        return view('departamento.index', ['departamentos' =>$departamentos]);
+
+
+
     }
 
     /**
