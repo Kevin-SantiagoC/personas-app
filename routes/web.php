77b0b0bc32e89ae48745ae25f8436bddf5927818
Kevin-SantiagoC <santiagocastano47@gmail.com>
2024-10-03 23:25:02 -0500

    Departamento Delete

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,6 @@ Route::post('/departamentos',[DepartamentoController::class, 'store'])->name('de
 
 Route::get('/departamentos/create',[DepartamentoController::class, 'create'])->name('departamentos.create');
 
+Route::delete('/departamentos/{departamento}',[DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
+
+
